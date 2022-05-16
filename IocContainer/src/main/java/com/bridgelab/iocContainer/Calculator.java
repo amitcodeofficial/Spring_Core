7083f4c6e8e880190b4858cba2b4a50ad6ce8427
Amit Dnyandeo Haral <amitharal04@gmail.com>
2022-05-16 21:32:08 +0530

    Delete IocContainer directory

--- IocContainer/src/main/java/com/bridgelab/iocContainer/Calculator.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelab.iocContainer;
-
-public interface Calculator {
-    public void UI();
-    public void addition(int a, int b);
-    public void subtraction(int a, int b);
-    public void division(int a, int b);
-    public void multiplication(int a, int b);
-}
